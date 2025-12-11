d32279983c34144228d95a634f99a5d4e4b70319
dsokol3 <dsokol3@student.touro.edu>
2025-12-11 16:49:58 -0500

    RS1F2-308  currupt city

--- recipeshare/src/main/java/com/mcon152/recipeshare/repository/TagRepository.java
+++ recipeshare/src/main/java/com/mcon152/recipeshare/repository/TagRepository.java
@@ -1,13 +1,13 @@
 package com.mcon152.recipeshare.repository;
 
-import com.mcon152.recipeshare.domain.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import com.mcon152.recipeshare.domain.Tag;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
+// @Repository
+// public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByName(String name);
     boolean existsByName(String name);
 }
