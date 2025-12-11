b884c7150cd50a6f26b4770655c16cafe9d5db89
egoldfar <egoldfar@student.touro.edu>
2025-12-11 16:50:47 -0500

    Uncomment main method to enable application startup

--- recipeshare/src/main/java/com/mcon152/recipeshare/RecipeShareApplication.java
+++ recipeshare/src/main/java/com/mcon152/recipeshare/RecipeShareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeShareApplication {
 
-    // public static void main(String[] args) {
-    //     SpringApplication.run(RecipeShareApplication.class, args);
-    // }
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeShareApplication.class, args);
+    }
 
 }
