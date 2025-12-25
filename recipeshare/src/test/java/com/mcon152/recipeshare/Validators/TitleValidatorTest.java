42d04784c6615701ba4e85300198fc201cd104ef
SKIND0 <skind@student.touro.edu>
2025-12-25 16:45:34 -0500

    started first test

---
+++ recipeshare/src/test/java/com/mcon152/recipeshare/Validators/TitleValidatorTest.java
@@ -0,0 +1,15 @@
+package com.mcon152.recipeshare.Validators;
+
+import com.mcon152.recipeshare.domain.*;
+import com.mcon152.recipeshare.web.RecipeRequest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TitleValidatorTest {
+
+    RecipeRequest recipe = new RecipeRequest();
+    recipe.setTitle()
+
+}
